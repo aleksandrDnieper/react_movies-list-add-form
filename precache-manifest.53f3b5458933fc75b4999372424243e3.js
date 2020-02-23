d229ec6e13694f9c59da666a8ec3390f7faef0ce
aleksandrDnieper <aleksandr.dnieper@gmail.com>
2020-02-23 18:36:50 +0200

    Updates

---
+++ precache-manifest.53f3b5458933fc75b4999372424243e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6c640bff3705473cd914178083b35d6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d78e7c3fa166cdccbdf6",
+    "url": "./static/css/2.70d9d258.chunk.css"
+  },
+  {
+    "revision": "dfb3d2b65d4e3a84ec81",
+    "url": "./static/css/main.b3efff03.chunk.css"
+  },
+  {
+    "revision": "d78e7c3fa166cdccbdf6",
+    "url": "./static/js/2.bb3638ba.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "./static/js/2.bb3638ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfb3d2b65d4e3a84ec81",
+    "url": "./static/js/main.cebcd745.chunk.js"
+  },
+  {
+    "revision": "bd651092f6acbe222378",
+    "url": "./static/js/runtime-main.14f95850.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2088c32903613710f8f9678a01b40cd4.js"
+  "./precache-manifest.53f3b5458933fc75b4999372424243e3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
